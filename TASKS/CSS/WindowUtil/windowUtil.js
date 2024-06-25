f9effb4b05d0f55ae948be62f83a4923c318cb3b
tskbalaji <tskbalaji15@gmail.com>
2024-06-25 20:45:24 +0530

    TasksCommentUpdated

---
+++ TASKS/CSS/WindowUtil/windowUtil.js
@@ -0,0 +1,13 @@
+// window.alert("Hi There!!!!");
+
+// var yourName = window.prompt("What is your name?");
+
+// console.log(yourName);
+
+// var yourAge = window.prompt("What is your age?");
+// var yoursAge = parseInt(yourAge);
+// console.log(yourAge);
+// console.log(typeof yourAge);
+// console.log(typeof yoursAge);
+
+window.confirm("Are you sure?");
--- TASKS/Task 7/RestCountriesFilter.js
+++ TASKS/Task 7/RestCountriesFilter.js
@@ -1,33 +1,35 @@
-  
-  const req = new XMLHttpRequest();
-  req.open("GET","https://restcountries.com/v3.1/all");
-  req.send();
-  req.onload = function () {
-    const obj = JSON.parse(this.response);
-    console.log(obj);
+const req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v3.1/all");
+req.send();
+req.onload = function () {
+  const obj = JSON.parse(this.response);
+  console.log(obj);
 
-    //Asian countries
-    asianCountries = obj.filter((element) => element.region == "Asia")
-    console.log(asianCountries);
-    
-    //Countries with population less than 2L.
-    countriesWithPopLessThan2L = obj.filter((element) => element.population < 200000)
-    console.log(countriesWithPopLessThan2L);
+  //Asian countries
+  asianCountries = obj.filter((element) => element.region == "Asia");
+  console.log(asianCountries);
 
-    //For Each - Name, Capital, Flag,
+  //Countries with population less than 2L.
+  countriesWithPopLessThan2L = obj.filter(
+    (element) => element.population < 200000
+  );
+  console.log(countriesWithPopLessThan2L);
 
-    obj.forEach(element => {
-      console.log(element.name.common);
-      console.log(element.capital);
-      console.log(element.flags.png);
-      
-    });
+  //For Each - Name, Capital, Flag,
 
-    //Total population.
-    totalPopulation = obj.reduce((acc, country) => acc + country.population , 0)
-    console.log(totalPopulation);
+  obj.forEach((element) => {
+    console.log(element.name.common);
+    console.log(element.capital);
+    console.log(element.flags.png);
+  });
 
-    //Countries using USD currency.
-    countiresUsingUSD = obj.filter((country) => country.currencies && country.currencies.USD)
-    console.log(countiresUsingUSD);
-  }
+  //Total population.
+  totalPopulation = obj.reduce((acc, country) => acc + country.population, 0);
+  console.log(totalPopulation);
+
+  //Countries using USD currency.
+  countiresUsingUSD = obj.filter(
+    (country) => country.currencies && country.currencies.USD
+  );
+  console.log(countriesUsingUSD);
+};
