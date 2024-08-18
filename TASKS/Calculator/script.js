9cb5e8d1151cf0a021891777b43f4425f3f5ec2e
tsk-balaji <tskbalaji15@gmail.com>
2024-08-19 01:43:13 +0530

    test-suite-changes

--- TASKS/Calculator/script.js
+++ TASKS/Calculator/script.js
@@ -1,97 +1,53 @@
-const currDisplay = document.querySelector(".curr-display");
-const prevDisplay = document.querySelector(".prev-display");
-const numbers = document.querySelectorAll(".number");
-const operands = document.querySelectorAll(".operation");
-const clearBtn = document.querySelector(".clear");
-const backspaceBtn = document.querySelector(".backspace");
-const equalBtn = document.querySelector(".equal");
-let operation;
-
-function appendNumber(number) {
-  if (number === "." && currDisplay.value.includes(".")) return;
-  currDisplay.value += number;
-}
-
-function chooseOperation(operand) {
-  if (currDisplay.value === "") return;
-  if (prevDisplay.innerText === "") {
-    prevDisplay.innerText = currDisplay.value + operand;
-  } else {
-    compute();
-    prevDisplay.innerText = currDisplay.value + operand;
-  }
-  operation = operand;
-  currDisplay.value = "";
-}
-
-function clearDisplay() {
-  currDisplay.value = "";
-  prevDisplay.innerText = "";
-}
-
-function compute(operand) {
-  let result;
-  const previousValue = parseFloat(prevDisplay.innerText);
-  const currentValue = parseFloat(currDisplay.value);
-
-  if (isNaN(previousValue) || isNaN(currentValue)) return;
-
-  switch (operation) {
-    case "+":
-      result = previousValue + currentValue;
-      break;
-    case "-":
-      result = previousValue - currentValue;
-      break;
-    case "*":
-      result = previousValue * currentValue;
-      break;
-    case "/":
-      result = previousValue / currentValue;
-      break;
-    default:
-      return;
-  }
-  currDisplay.value = result;
-}
-
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    appendNumber(number.innerText);
-  });
-});
-
-operands.forEach((operand) => {
-  operand.addEventListener("click", () => {
-    chooseOperation(operand.innerText);
-  });
-});
-
-clearBtn.addEventListener("click", () => {
-  clearDisplay();
-});
-
-equalBtn.addEventListener("click", () => {
-  compute();
-  prevDisplay.innerText = "";
-});
-
-backspaceBtn.addEventListener("click", () => {
-  currDisplay.value = currDisplay.value.slice(0, -1);
-});
-
-document.addEventListener("keydown", (event) => {
-  const key = event.key;
-  if ((key >= 0 && key <= 9) || key === ".") {
-    appendNumber(key);
-  } else if (key === "+" || key === "-" || key === "*" || key === "/") {
-    chooseOperation(key);
-  } else if (key === "Enter") {
-    compute();
-    prevDisplay.innerText = "";
-  } else if (key === "Backspace") {
-    currDisplay.value = currDisplay.value.slice(0, -1);
-  } else {
-    alert("Only numbers and operations are allowed");
-  }
-});
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pagination</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <h1 id="title" class="title" style="color: white; padding-bottom: 15px">
+        Calculator
+      </h1>
+      <p
+        id="description"
+        class="description"
+        style="color: white; padding-bottom: 15px"
+      >
+        Personal details will be rendered as per the Id clicked in the
+        pagination
+      </p>
+      <div class="data">
+        <div
+          class="table-responsive"
+          style="color: white; text-align: center; padding-top: 150px"
+          ;
+        >
+          Please click on the desired Id below for their details
+        </div>
+      </div>
+      <div class="pagination">
+        <button class="btn3" onclick="prevSet() ">⇤</button>
+        <button class="btn1" onclick="backBtn()">prev</button>
+        <ul>
+          <li class="link" value="1">1</li>
+          <li class="link" value="2">2</li>
+          <li class="link" value="3">3</li>
+          <li class="link" value="4">4</li>
+          <li class="link" value="5">5</li>
+          <li class="link" value="6">6</li>
+          <li class="link" value="7">7</li>
+          <li class="link" value="8">8</li>
+          <li class="link" value="9">9</li>
+          <li class="link" value="10">10</li>
+        </ul>
+        <button class="btn2" onclick="nextBtn()">next</button>
+        <button class="btn4" onclick="nextSet()">⇥</button>
+      </div>
+    </div>
+    <script src="script.js"></script>
+    <script src="https://app.zenclass.in/sheets/v1/js/zen/suite/bundle.js"></script>
+  </body>
+</html>
