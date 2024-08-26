2f6e8af89e64dabcc098904836f07d5d755542e8
tsk-balaji <tskbalaji15@gmail.com>
2024-08-26 20:24:12 +0530

    GOT  Quotes

---
+++ TASKS/Promise Tasks/GOT Quotes/script.js
@@ -0,0 +1,23 @@
+function fetchAdvice() {
+  fetch("https://api.gameofthronesquotes.xyz/v1/random")
+    .then((response) => response.json())
+    .then((data) => {
+      var sentence = data.sentence;
+      var character = data.character.name;
+      const quoteDiv = document.createElement("div");
+      quoteDiv.classList.add("quote");
+      const byDiv = document.createElement("div");
+      byDiv.classList.add("by");
+      quoteDiv.innerText = sentence;
+      byDiv.innerText = ` -  ${character}`;
+      quoteDiv.appendChild(byDiv);
+      document.body.appendChild(quoteDiv);
+    })
+    .catch((error) => {
+      console.error("Error fetching advice:", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAdvice();
+});
